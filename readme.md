d9c1675a7c9a226acea0edb0a481e6db4abc6b24
uaimmiau <zoltanchivay2137@gmail.com>
2025-01-25 14:56:10 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Setup:
+
+-   Install anaconda (`https://www.anaconda.com/download`) and import the environment using `ao_projekt.yaml`.
+-   Set a correct abosolute path to python on anaconda in `config.json`.
+-   Install node.js dependencies using `npm i`.
+-   Start server `node server.js`.
+
+# Using:
+
+-   Navigate to `http://localhost:3000/`.
+-   Choose an image via Choose File input.
+-   Mark clipping region by holding mouse down dragging and releasing.
+-   Click `Clip and Submit` and wait for server response.
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const multer = require("multer");
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const config = require("./config.json");
+
+const app = express();
+const PORT = 3000;
+
+const IMG_FOLDER = "output";
+if (!fs.existsSync(IMG_FOLDER)) {
+    fs.mkdirSync(IMG_FOLDER);
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, IMG_FOLDER);
+    },
+    filename: (req, file, cb) => {
+        cb(null, "your_image.png");
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/clip", upload.single("clippedImage"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send("No clipped image uploaded");
+    }
+
+    const command = `${config.condaPath} run -n ao_projekt python demo.py --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn --image_folder output --saved_model best_accuracy.pth`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error("Error executing script:", stderr);
+            return res.status(500).send("Error processing the file");
+        }
+
+        res.send(`${stdout}`);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
